7f3dc89b976b517914c93ad31c12b11f90e19f70
vudinhdai <vudinhdai1999hnue@gmail.com>
2019-08-13 09:47:31 +0700

    cm

---
+++ src/FizzBuzz.java
@@ -0,0 +1,32 @@
+/*public class FizzBuzz {
+        public static String fizzBuzz(int number){
+            if(number % 3 == 0 && number % 5 == 0)
+                return "FizzBuzz";
+
+            if(number % 3 == 0)
+                return "Fizz";
+
+            if(number % 5 == 0)
+                return "Buzz";
+
+            return number + "";
+        }
+    }*/
+public class FizzBuzz {
+    public static String fizzBuzz(int number) {
+        boolean isFizz = number % 3 == 0;
+        boolean isBuzz = number % 5 == 0;
+
+        if (isFizz && isBuzz)
+            return "FizzBuzz";
+
+        if (isFizz)
+            return "Fizz";
+
+        if (isBuzz)
+            return "Buzz";
+
+        return number + "";
+    }
+}
+
